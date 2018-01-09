52ba66f1393d15de96975618ce6579475c7f9eba
Tarcísio Marinho <tarcisio_marinho09@hotmail.com>
2018-01-09 16:42:35 -0300

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -49,6 +49,7 @@ sudo pip3 install requests
 sudo pip3 install matplotlib
 sudo pip3 install numpy
 sudo pip3 install pandas
+sudo pip3 install jupyter
 
 
 sudo apt-get install indicator-multiloader
